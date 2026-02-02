3a812724b0ce7f4cd4f85bebf89962b99ec7bf03
Neks95 <rakotomangaa.nekena@gmail.com>
2026-02-02 14:07:10 +0300

    base de donne

---
+++ db/script.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS boot;
+USE boot;
+
+CREATE TABLE IF NOT EXISTS user(
+    id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(75) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS etat_message(
+    id_etat INT PRIMARY KEY AUTO_INCREMENT,
+    etat VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS message(
+    id_message INT PRIMARY KEY AUTO_INCREMENT,
+    id_user_envoyeur INT NOT NULL,
+    id_user_recepteur INT NOT NULL,
+    contenu TEXT NOT NULL,
+    id_etat INT NOT NULL DEFAULT 1,
+    date_envoi TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_lecture TIMESTAMP NULL,
+    
+    FOREIGN KEY (id_user_envoyeur) REFERENCES user(id_utilisateur) ON DELETE CASCADE,
+    FOREIGN KEY (id_user_recepteur) REFERENCES user(id_utilisateur) ON DELETE CASCADE,
+    FOREIGN KEY (id_etat) REFERENCES etat_message(id_etat)
+);
+
+
+INSERT INTO etat_message (etat) VALUES 
+('non_lu'),
+('lu');
